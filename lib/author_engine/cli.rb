cb51e68f74d7fba49180ff8a4aad6dd541504821
Cyberarm <matthewlikesrobots@gmail.com>
2018-12-04 16:40:40 -0600

    Got opal export almost working. It's incorrectly placing author_engine runtime before Opal is properly defined, or I'm screwing it up somehow; Opal's documentation leaves MUCH to be desired...

--- lib/author_engine/cli.rb
+++ lib/author_engine/cli.rb
@@ -3,7 +3,8 @@ class AuthorEngine
     def initialize
       if ARGV[0] && ARGV[0] == "export"
         if ARGV[1] && ARGV[1].end_with?(".authorengine")
-          require "author_engine/opal"
+          require "author_engine/game/opal/exporter"
+
           AuthorEngine::OpalExporter.new(project_file: ARGV[1])
         else
           puts "author_engine export project [exported_name]"
--- lib/author_engine/opal.rb
+++ lib/author_engine/opal.rb
@@ -1,14 +1,9 @@
-require "opal"
-require "opal-parser"
+raise "Only require \"author_engine/opal\" with Opal!" unless RUBY_ENGINE == "opal"
 
-require_relative "game/opal/common"
-require_relative "game/opal/colors"
-require_relative "game/opal/graphics"
-require_relative "game/opal/input"
+require_relative "game/opal/parts/common"
+require_relative "game/opal/parts/colors"
+require_relative "game/opal/parts/graphics"
+require_relative "game/opal/parts/input"
 
 require_relative "game/game"
 require_relative "game/opal/game_runner"
-
-unless RUBY_ENGINE == "opal"
-  require_relative "game/opal/exporter"
-end
