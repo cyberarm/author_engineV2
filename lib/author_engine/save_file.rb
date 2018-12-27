68e2f5bc2b32aa557e79b2a698e7bc9ee188f5ba
Cyberarm <matthewlikesrobots@gmail.com>
2018-12-27 11:29:48 -0600

    Fixed crash when savefile is first created

--- lib/author_engine/save_file.rb
+++ lib/author_engine/save_file.rb
@@ -130,7 +130,7 @@ class AuthorEngine
     end
 
     def identify_mode(string)
-      if string.lines.first.include?("# inflated")
+      if string.lines.size > 0 && string.lines.first.include?("# inflated")
         @mode = :inflated
       else
         @mode = :compact
